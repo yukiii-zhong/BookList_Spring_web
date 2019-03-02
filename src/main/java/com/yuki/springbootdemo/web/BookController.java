0eaaee5619460975d109292c4b631d60f0d78619
yukizhong <yukiiizhong@gmail.com>
2019-03-01 21:23:12 -0500

    Edit input function and support update

--- src/main/java/com/yuki/springbootdemo/web/BookController.java
+++ src/main/java/com/yuki/springbootdemo/web/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -33,5 +34,31 @@ public class BookController {
         }
         model.addAttribute("book", book);
         return "book";
+    }
+
+    /**
+     * 跳转input提交页面
+     * @return
+     */
+    @GetMapping("/books/input")
+    public String inputPage(Model model){
+        model.addAttribute("book", new Book());
+        return "input";
+    }
+
+    @GetMapping("/books/input/{id}")
+    public String inputEditPage(@PathVariable long id, Model model){
+        Book book=bookService.getOne(id);
+        model.addAttribute("book", book);
+        return "input";
+    }
+
+    /**
+     * 提交一个书单信息
+     */
+    @PostMapping("/books")
+    public String post(Book book){
+        bookService.save(book);
+        return "redirect:/v3/books";
     }
 }
