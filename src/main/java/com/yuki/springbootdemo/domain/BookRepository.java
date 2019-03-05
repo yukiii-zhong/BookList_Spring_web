287fe647ae22f3f4106d67a847aa62443311e668
yukizhong <yukiiizhong@gmail.com>
2019-03-05 17:45:19 -0500

    update

--- src/main/java/com/yuki/springbootdemo/domain/BookRepository.java
+++ src/main/java/com/yuki/springbootdemo/domain/BookRepository.java
@@ -1,5 +1,7 @@
 package com.yuki.springbootdemo.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +10,8 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findAll(Pageable pageable);
 
     List<Book> findByAuthor(String author);
 
--- src/main/java/com/yuki/springbootdemo/service/BookService.java
+++ src/main/java/com/yuki/springbootdemo/service/BookService.java
@@ -3,6 +3,8 @@ package com.yuki.springbootdemo.service;
 import com.yuki.springbootdemo.domain.Book;
 import com.yuki.springbootdemo.domain.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.Id;
@@ -20,6 +22,14 @@ public class BookService {
     public List<Book> findAll(){
         return bookRepositry.findAll();
     }
+
+
+//    public Page<Book> findAllbyPage(){
+//
+//        Pageable pageable = new Pageable();
+//        return bookRepositry.findAll();
+//    }
+
     // 新增一个书单
     public Book save(Book book){
         return bookRepositry.save(book);
